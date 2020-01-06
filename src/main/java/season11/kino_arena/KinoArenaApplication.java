5cef34c23898a95906bdc57f42c437478b58a24a
AlexanderKonov <alexander.konov@abv.bg>
2020-01-06 20:35:55 +0200

    new project setup

---
+++ src/main/java/season11/kino_arena/KinoArenaApplication.java
@@ -0,0 +1,13 @@
+package season11.kino_arena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinoArenaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KinoArenaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/season11/kino_arena/KinoArenaApplicationTests.java
@@ -0,0 +1,13 @@
+package season11.kino_arena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinoArenaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
