c227a2e1124358d25e6521ea6bc85121fceee988
Dimitar <dimitari.nikolov@gmail.com>
2020-01-06 20:00:02 +0200

    creating spring project

---
+++ KinoArena/src/test/java/java11/cinemas/CinemasApplicationTests.java
@@ -0,0 +1,13 @@
+package java11.cinemas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
