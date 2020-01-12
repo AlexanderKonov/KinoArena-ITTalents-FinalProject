e6643ba10f141f073b83ab946cad8ea3b90f05af
AlexanderKonov <alexander.konov@abv.bg>
2020-01-12 15:30:53 +0200

    Repositories added.

--- src/main/java/season11/kino_arena/model/repository/RestrictionRepository.java
+++ src/main/java/season11/kino_arena/model/repository/RestrictionRepository.java
@@ -2,8 +2,8 @@ package season11.kino_arena.model.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import season11.kino_arena.model.pojo.Cinema;
+import season11.kino_arena.model.pojo.Restriction;
 
 @Repository
-public interface CinemaRepository extends JpaRepository<Cinema, Long> {
+public interface RestrictionRepository extends JpaRepository<Restriction, Long> {
 }
--- src/main/java/season11/kino_arena/model/repository/UserRepository.java
+++ src/main/java/season11/kino_arena/model/repository/UserRepository.java
@@ -2,8 +2,8 @@ package season11.kino_arena.model.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import season11.kino_arena.model.pojo.Cinema;
+import season11.kino_arena.model.pojo.User;
 
 @Repository
-public interface CinemaRepository extends JpaRepository<Cinema, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
