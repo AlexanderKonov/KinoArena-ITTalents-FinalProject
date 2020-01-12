74efd67a417af9dcd8267846e5f4dcadc6604968
Dimitar <dimitari.nikolov@gmail.com>
2020-01-12 14:39:56 +0200

    added hibernate and some annotations to pojos

--- src/main/java/season11/kino_arena/model/pojo/Cinema.java
+++ src/main/java/season11/kino_arena/model/pojo/Cinema.java
@@ -6,17 +6,27 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "cinemas")
 public class Cinema {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @Column
     private String name;
+    @Column
     private String address;
+    @Column(name = "telephone_number")
     private String telephoneNumber;
+    @Column(name = "cinema_info")
     private String cinemaInfo;
+    @Column
     private String city;
 
 }
--- src/main/java/season11/kino_arena/model/pojo/CinemaHall.java
+++ src/main/java/season11/kino_arena/model/pojo/CinemaHall.java
@@ -10,17 +10,27 @@ import season11.kino_arena.model.dao.CinemaDAO;
 import season11.kino_arena.model.dao.CinemaHallTypeDAO;
 import season11.kino_arena.model.dto.CinemaHallDTO;
 
+import javax.persistence.*;
 import java.sql.SQLException;
 
 @Getter
 @Setter
 @NoArgsConstructor
+@Entity
+@Table(name = "cinema_halls")
 public class CinemaHall {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @ManyToOne
+    @JoinColumn(name = "cinema_hall_type_id")
     private CinemaHallType cinemaHallType;
+    @ManyToOne
+    @JoinColumn(name = "cinema_id")
     private Cinema cinema;
+    @Column(name = "number_of_rows")
     private int numberOfRows;
+    @Column(name = "number_of_seats_per_row")
     private int numberOfSeatsPerRow;
 
     public CinemaHall(CinemaHallDTO hall, CinemaHallType cinemaHallType, Cinema cinema) {
